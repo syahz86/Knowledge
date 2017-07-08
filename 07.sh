17a6af6c25eab61c6ee2b52edece41e04f187127
Tony Esterdark <messi.klate@gmail.com>
2017-07-09 01:53:14 +0800

    Update 07.sh

--- 07.sh
+++ 07.sh
@@ -1,13 +1,13 @@
 #
 #!/bin/bash
 #Script hapus akaun SSH/VPN
-#Doctype | Malaysian Phreaker Knowledge
+#Syahz86 | GollumVPN
 #
 
 read -p "Username Account going to be delete: " User
 userdel -r $User
 
 echo "==============================================="
-echo "Doctype | Malaysian Phreaker Knowledge | FrogyX"
+echo "Syahz86 | GollumVPN | Si Tony"
 echo "==============================================="
 echo ""
